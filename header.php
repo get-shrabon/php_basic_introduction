e16b0852b2fafdfa91b575992bdc9bd7ab24818a
Shrabon Talukder <getshrabon@gmail.com>
2024-04-28 18:27:54 +0600

    Change some code

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
     <ul class="nav-items">
         <li><a href="index.php">Home</a></li>
         <li><a href="about.php">About</a></li>
-        <li><a href="index.php">Service</a></li>
+        <li><a href="service.php">Service</a></li>
         <li><a href="index.php">Contact</a></li>
     </ul>
 </header>
--- service.php
+++ service.php
@@ -1,29 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Page</title>
+    <title>Service Page</title>
     <link rel="stylesheet" href="style.css">
     <style>
-        .about{
+        .service {
             width: 100%;
             height: 100vh;
-            background-color: aquamarine;
+            background-color: darkcyan;
         }
-        .about header{
+
+        .service header {
             background-color: bisque;
             padding: 20px 0;
         }
-        h1{
+
+        h1 {
             text-align: center;
             padding-top: 100px;
             font-size: 70px;
         }
     </style>
 </head>
-<body class="about">
-    <?php require_once ("header.php") ?>
-    <h1>About Page</h1>
+
+<body class="service">
+    <?php require_once("header.php") ?>
+    <h1>Service Page</h1>
 </body>
+
 </html>
